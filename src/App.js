010e3ca047c887cdf789420ce88786e8486ce12f
f0restdmg <dimonatr@mail.ru>
2021-01-30 01:01:14 +0400

    push to main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import Slider from './components/slider/Slider';
+
+let slides = [
+  {
+    img: "https://www.w3schools.com/howto/img_nature_wide.jpg",
+    text: "Caption Text 1",
+  },
+  {
+    img: "https://www.w3schools.com/howto/img_snow_wide.jpg",
+    text: "Caption Text 2",
+  },
+  {
+    img: "https://www.w3schools.com/howto/img_mountains_wide.jpg",
+    text: "Caption Text 3",
+  },
+]
 
 const App = () => {
   return (
-    <div>
-      hello world
-    </div>
+    <>
+      <Slider slides={slides} loop={true} navs={true} auto={true} pags={true} delay={3} />
+    </>
   );
 }
 
